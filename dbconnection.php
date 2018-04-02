bc96d85f0eedab81d2a6b5db23d6acdd595240ff
Prathamesh Kotgire <prathkotgire@gmail.com>
2018-04-02 13:05:34 -0400

    Added login and registration
    Functions to register and login were added. Both functions return a boolean based on their success.

--- dbconnection.php
+++ dbconnection.php
@@ -11,16 +11,45 @@
         $this->createUsersTable();
       }
 
+      // Creates the 'users' table in the blog db if it does not exist
       private function createUsersTable() {
-        $query = "CREATE TABLE 'users' ('username' varchar(16) NOT NULL, 'email'
-          varchar(100) NOT NULL, 'password' varchar(255) NOT NULL, 'firstname'
-          varchar(32) NOT NULL, 'lastname' varchar(32) NOT NULL, 'birthday'
-          date NOT NULL, 'website' varchar(255) NOT NULL,
-          PRIMARY KEY ('username'))";
+        $query = "CREATE TABLE users (username varchar(16) NOT NULL, email
+          varchar(100) NOT NULL, password varchar(255) NOT NULL, firstname
+          varchar(32) NOT NULL, lastname varchar(32) NOT NULL, birthday
+          date, website varchar(255) NOT NULL,
+          PRIMARY KEY (username));";
+
+          $this->runQuery($query);
       }
 
+      // Function to register a user into the 'users' databse
+      // Returns a boolean based on success
+      public function register($email, $username, $password) {
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        $query = "INSERT INTO users (email, username, password)
+          VALUES ('$email', '$username', '$password')";
+        return $this->runQuery($query);
+      }
+
+      // Function to check if login is valid
+      // Returns a boolean based on success
+      public function login($username, $password) {
+        $query = "SELECT username, password FROM users WHERE username = '$username'";
+        $result = $this->runQuery($query);
+
+        if ($result->num_rows > 0) {
+          $row = $result->fetch_assoc();
+          if (password_verify($password, $row["password"])) {
+            return TRUE;
+          }
+        }
+
+        return FALSE;
+      }
+
+      // Function to run a query, returns the result
       private function runQuery($query) {
-        $conn = new mysqli($servername, $username, $password, $dbName);
+        $conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
 
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
