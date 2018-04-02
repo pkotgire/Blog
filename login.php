c166135ed74521c8774054776ce011f5ede6af5a
aminamazhar <35942843+aminamazhar@users.noreply.github.com>
2018-04-02 13:02:55 -0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,35 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+		<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+		<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+
+	</head>
+        
+    <body>
+        
+    	<div id="tinypage"> <!--The id is for the small sign up box on the exmaple blog website-->
+    		<h1>Login</h1>
+    		<form action="register.html" method="post">
+    		<a>Username</a><br><input type="text" name="username"></input><br>
+    		<a>Password</a><br><input type="password" name="password"></input><br>
+    		<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">Login</button></form>
+
+    	</div>
+        
+        <div id="footer">
+        	<footer>
+  				<a href="http://www.yahoo.com">signup</a>
+  				<a href="http://www.yahoo.com">login</a>
+  				<a href="http://www.yahoo.com">about</a>
+			</footer>
+        </div>
+
+	
+	
+	</body>
+
+</html>
