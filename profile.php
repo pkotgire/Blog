3f52e87c062839f0f60def8ab9e08fe69ee56d12
Ayyub <ayyubb05@gmail.com>
2018-04-03 09:43:16 -0400

    fixed more undefined variables

--- profile.php
+++ profile.php
@@ -12,6 +12,11 @@
   // fetch user info from database
   $username = $_SESSION["username"];
   $user = $db->getUserInfo($username);
+  $email = $user["email"];
+  $fname = $user["firstname"];
+  $lname = $user["lastname"];
+  $bday = $user["birthday"];
+  $website = $user["website"];
 
 
   $body = <<<EOBODY
@@ -20,9 +25,9 @@
         <tbody>
           <tr><td><strong>Username: </strong></td><td>$username</td></tr>
           <tr><td><strong>Email: </strong></td><td>$email</td></tr>
-          <tr><td><strong>First Name: </strong></td><td>$firstname</td></tr>
-          <tr><td><strong>Last Name: </strong></td><td>$lastname</td></tr>
-          <tr><td><strong>Birthday: </strong></td><td>$birthday</td></tr>
+          <tr><td><strong>First Name: </strong></td><td>$fname</td></tr>
+          <tr><td><strong>Last Name: </strong></td><td>$lname</td></tr>
+          <tr><td><strong>Birthday: </strong></td><td>$bday</td></tr>
           <tr><td><strong>Website: </strong></td><td>$website</td></tr>
         </tbody>
       </table>
