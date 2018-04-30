0d87f096fa01082ccc0bec61255026bc26388f74
Prathamesh Kotgire <prathkotgire@gmail.com>
2018-04-30 17:54:24 -0400

    fix to generate_guid function

--- dbconnection.php
+++ dbconnection.php
@@ -133,10 +133,10 @@
               mt_srand((double)microtime()*10000);
               $charid = strtoupper(md5(uniqid(rand(), true)));
               $hyphen = chr(45); // "-"
-              $uuid = substr($charid, 0, 8).$hyphen
-                  .substr($charid, 8, 4).$hyphen
-                  .substr($charid,12, 4).$hyphen
-                  .substr($charid,16, 4).$hyphen
+              $uuid = substr($charid, 0, 8)
+                  .substr($charid, 8, 4)
+                  .substr($charid,12, 4)
+                  .substr($charid,16, 4)
                   .substr($charid,20,12);
               return $uuid;
           }
